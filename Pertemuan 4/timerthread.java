1fe834343d7db331a8680cbe2aaf447d2a1dde37
SuryaAndyartha <suryanyoman6@gmail.com>
2025-09-22 18:09:50 +0700

    Update timerthread.java

--- Pertemuan 4/timerthread.java
+++ Pertemuan 4/timerthread.java
@@ -1,22 +1,21 @@
-public class TimerThread extends Thread {
+public class TimerThread extends Thread{
     private Clock clock;
 
-    public TimerThread(Clock clock) {
+    public TimerThread(Clock clock){
         this.clock = clock;
     }
 
-    public void run() {
-        while (clock.isRunning()) {
+    public void run(){
+        while (clock.isRunning()){
             clock.step();
             pause();
         }
     }
 
-    private void pause() {
-        try {
+    private void pause(){
+        try{
             Thread.sleep(1000);
-        } catch (InterruptedException exc) {
-            // ignore
-        }
+        } 
+        catch(InterruptedException exc){}
     }
 }
