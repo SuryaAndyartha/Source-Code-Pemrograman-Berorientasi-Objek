c70ee942ea533b4a07e576ae4c1dba17d2859971
SuryaAndyartha <suryanyoman6@gmail.com>
2025-09-29 22:12:38 +0700

    Create Mahasiswa.java

---
+++ Pertemuan 5/Mahasiswa.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Mahasiswa{
+    private String nama;
+    private String nrp;
+    private ArrayList<FRSItem> frs;
+
+    public Mahasiswa(String nama, String nim){
+        this.nama = nama;
+        this.nrp = nrp;
+        this.frs = new ArrayList<>();
+    }
+
+    public void ambilMatkul(MataKuliah matkul, Dosen dosen){
+        FRSItem item = new FRSItem(matkul, dosen);
+        if(!frs.contains(item)){  
+            frs.add(item);
+            System.out.println(nama + " berhasil mengambil " + matkul.getNama() + " dengan " + dosen.getNama());
+        } 
+        else{
+            System.out.println("Mata kuliah sudah diambil!");
+        }
+    }
+
+    public void tampilkanFRS(){
+        System.out.println("\nFRS " + nama + " (" + nrp + "):");
+        for (FRSItem item : frs) {
+            System.out.println("- " + item);
+        }
+    }
+}
