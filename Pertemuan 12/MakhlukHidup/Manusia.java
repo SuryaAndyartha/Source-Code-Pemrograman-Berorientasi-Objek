ba5f2bdacaf105b0317c3aa513a14163c943d909
SuryaAndyartha <suryanyoman6@gmail.com>
2025-11-30 13:36:33 +0700

    Create Manusia.java

---
+++ Pertemuan 12/MakhlukHidup/Manusia.java
@@ -0,0 +1,16 @@
+public class Manusia extends MakhlukHidup {
+
+    public Manusia(String nama){
+        super(nama);
+    }
+
+    @Override
+    public void bernafas(){
+        System.out.println(getNama() + " bernafas menggunakan paru-paru.");
+    }
+
+    @Override
+    public void berkembangBiak(){
+        System.out.println(getNama() + " berkembang biak secara melahirkan.");
+    }
+}
