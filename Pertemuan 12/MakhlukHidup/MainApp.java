86de4e8f3724ff8dc6d7ce6cfac2ed59cdc30767
SuryaAndyartha <suryanyoman6@gmail.com>
2025-11-30 13:37:09 +0700

    Create MainApp.java

---
+++ Pertemuan 12/MakhlukHidup/MainApp.java
@@ -0,0 +1,23 @@
+public class MainApp {
+    public static void main(String[] args) {
+        MakhlukHidup m1 = new Manusia("Andi");
+        MakhlukHidup m2 = new Hewan("Kucing");
+        MakhlukHidup m3 = new Tumbuhan("Mangga");
+
+        m1.info();
+        m1.bernafas();
+        m1.berkembangBiak();
+
+        System.out.println();
+
+        m2.info();
+        m2.bernafas();
+        m2.berkembangBiak();
+
+        System.out.println();
+
+        m3.info();
+        m3.bernafas();
+        m3.berkembangBiak();
+    }
+}
